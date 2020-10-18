f0396531c70a76bfe413ef1172a525e4a60c3690
Akshay Havale <akshayhavale22@gmail.com>
2020-10-18 07:54:47 +0530

    java scheduling job by cron job

--- spring-boot-task-scheduler/src/main/java/com/java/model/User.java
+++ spring-boot-task-scheduler/src/main/java/com/java/model/User.java
@@ -1,19 +1,24 @@
 package com.java.model;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
-@Data
-@NoArgsConstructor
-@Builder
+
+@Entity
+@Setter
+@Getter
 @AllArgsConstructor
+@ToString
+@NoArgsConstructor
 public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
--- spring-boot-task-scheduler/src/main/java/com/java/service/UserServiceImpl.java
+++ spring-boot-task-scheduler/src/main/java/com/java/service/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.java.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import com.java.model.User;
@@ -10,6 +11,8 @@ import com.java.repository.UserRepository;
 
 @Service
 public class UserServiceImpl implements UserService {
+
+	private final int count = 0;
 
 	@Autowired
 	private UserRepository repository;
@@ -21,11 +24,26 @@ public class UserServiceImpl implements UserService {
 
 	@Override
 	/**
-	 * the contract of task scheduling method is 
-	 * the method should not accept any arguments and it should not return anything
+	 * the contract of task scheduling method is the method should not accept any
+	 * arguments and it should not return anything
 	 */
+	@Scheduled(cron = "*/30 * * * * *")
 	public void createUserByScheduledTime() {
 
+		
+		int counter = increment(count);
+		
+		User user = new User();
+		user.setName("AKSHAY"+counter);
+
+		repository.save(user);
+
+		System.out.println(user);
+
+	}
+
+	public static int increment(int count) {
+		return count++;
 	}
 
 }
